7e76f7d4428e4c18184d9b42df7a67ebc1ac0171
Deaths-Door <aaravaditya51@gmail.com>
2022-10-14 19:26:47 +0200

    CAN CONVERT SAME TYPE

--- Convertor.kt
+++ Convertor.kt
@@ -2,13 +2,11 @@ class Convertor(val from :String, val to:String, var num:String){
     /**
      * All options and units and values for them
      * **/
-
     private val units = mapOf("Length" to Pair(arrayListOf("km","m","cm","mm"),arrayListOf(1000,100,10)))
 
     /**
      * Get All units of filtered
      * **/
-    val s = mapOf("l" to Pair(1,2))
     fun getUnits(s:String = ""): Map<String, Pair<ArrayList<String>, ArrayList<Int>>> {
         if(s == "") return units
         return units.filter {it.key == s }
@@ -17,15 +15,14 @@ class Convertor(val from :String, val to:String, var num:String){
     /**
      * if given parms are valid
      **/
+    private var fromType = ""
+    private var fromIndex = -1
+    private var toType   = ""
+    private var toIndex  = -1
 
     private fun isValidNum(): Boolean = num.matches("([+-]?(?=\\.\\d|\\d)(?:\\d+)?(?:\\.?\\d*))(?:[eE]([+-]?\\d+))?".toRegex())
-    var fromType = ""
-    var fromIndex = -1
-    var toType   = ""
-    var toIndex  = -1
-
     fun convert(): String {
-        if(!isValidNum()) return "Enter Valid Number"
+        if(!isValidNum()) return "INVALID NUMBER"
         if(from == to) return num
 
         for((key,value) in units){
@@ -45,17 +42,31 @@ class Convertor(val from :String, val to:String, var num:String){
         if(fromIndex == -1 || toIndex == -1) return "WRONG UNITS"
 
         //same type
-        if(fromType == toType) {
-            sameTypeConvert()
-        }
+        if(fromType == toType) sameTypeConvert()
+        else notSameTypeConvert()
         return num
     }
 
     private fun sameTypeConvert() {
+        val arr = units[fromType]!!.second
+        if(fromIndex > toIndex) arr.reverse()
+        var x = 1
         while(fromIndex != toIndex){
+            //TODO ADD CUSTOM MULTIPLY FUNCTION
             //Convert
-            
+            if(fromIndex < toIndex){
+                num = (num.toDouble() * arr[toIndex - 1]).toString()
+                toIndex--
+            }
+            else{
+                num = (num.toDouble() / arr[fromIndex - 1]).toString()
+                fromIndex--
+            }
         }
+    }
+
+    private fun notSameTypeConvert() {
+
     }
 }
 
